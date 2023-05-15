c974c2dcd41b52c28d43202b8e2c75f02ca8974f
Valeria Oreshko <oreshko-v@yandex.ru>
2023-05-15 20:05:46 +0300

    Second commit

--- src/main/java/ru/netology/Hwww/services/RestService.java
+++ src/main/java/ru/netology/Hwww/services/RestService.java
@@ -7,9 +7,9 @@ public class RestService {
         for (int month = 0; month < 12; month++) {
             if (money >= threshold) { // можем ли отдыхать?
                 count++; // увеличиваем счётчик месяцев отдыха
-                money = (money - expenses)/3;
+                money = (money - expenses) / 3;
             } else {
-                money = money + income -expenses;
+                money = money + income - expenses;
             }
         }
         return count;
--- src/test/java/ru/netology/Hwww/services/RestServiceTest.java
+++ src/test/java/ru/netology/Hwww/services/RestServiceTest.java
@@ -1,25 +1,16 @@
 package ru.netology.Hwww.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class RestServiceTest {
-    @Test
-    public void testFirst() {
-        RestService service = new  RestService();
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/Rest.csv")
+    public void testFirst(int expected, int income, int expenses, int threshold) {
+        RestService service = new RestService();
 
-        int expected = 3;
-        int actual = service.calculate(10_000, 3_000, 20_000);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testSecond() {
-        RestService service = new  RestService();
-
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
